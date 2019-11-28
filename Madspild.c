bc2ed9dbdc145d0f324db6a02dab71bf6c936242
Wrafttex <nkure19@student.aau.dk>
2019-11-28 10:50:21 +0100

    Updated 28/11 10:50

--- Madspild.c
+++ Madspild.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void readDataBase (void);
+
+#define MAX_NAME_LGT 500
+
+typedef struct 
+{
+   char rettensNavn[MAX_NAME_LGT];
+   char antalPersoner[MAX_NAME_LGT];
+   char forberedningsTid[MAX_NAME_LGT];
+   char Ingredienser[MAX_NAME_LGT];
+   char Fremgangsmaade[MAX_NAME_LGT];
+} retter;
+
 int main (void)
 {
-    int choice;
-   
+   int valg;
+   // retter ret[42] = {"","","","",""};
    /*Displaing on screen*/
    printf("-------Den Digitale Madplan-------\n");
    printf(" Alle madplaner gaelder for 7 dage,\n og inkludere inkoebsliste, opskrifter\n og dato for andvendelse\n\n");
@@ -14,21 +27,41 @@ int main (void)
    printf("3) Kylling\n");
  
    /*getting input*/
-   scanf("%d", &choice);
- 
-   system("CLS");
+   scanf("%d", &valg);
  
    /*Finding which choice was asked for (my style of using brackets may be different than yours*/
-   if (choice == 1){
+   if (valg == 1){
       printf("Du valgte en vegansk madplan!\n");
    }
-   else if (choice == 2){
+   else if (valg == 2){
       printf("Du valgte en proteinbaseret madplan!\n");
    }
-   else if (choice == 3){
+   else if (valg == 3){
       printf("Du valgte en madplan baseret paa kylling!\n");
    }
  
-   system("PAUSE");
+   readDataBase();
+   
    return 0;
+}
+
+/* reads from files */
+void readDataBase (void)
+{
+   retter ret;
+   FILE* Opskrifter;
+
+   Opskrifter = fopen("FamilieMadplan.txt", "r");
+
+   if (Opskrifter == NULL) {
+      printf("Error\n");
+      EXIT_FAILURE;
+   }
+
+   fscanf(Opskrifter, "%*s %*s %*s %[^;]; %[^;]; %[^;]; %[^&]& %[^&]&", &ret.rettensNavn, &ret.antalPersoner, &ret.forberedningsTid, &ret.Ingredienser, &ret.Fremgangsmaade);
+   printf("%s\n \n%s\n \n%s\n \n%s \n%s", ret.rettensNavn, ret.antalPersoner, ret.forberedningsTid, ret.Ingredienser, ret.Fremgangsmaade);
+
+
+
+   fclose(Opskrifter);
 }
